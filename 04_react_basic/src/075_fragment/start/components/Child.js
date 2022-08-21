0a15ab4af17c7b0660f3a1681229c26c7037b25d
ユーザ名 <sayasurvey@yahoo.co.jp>
2022-08-21 13:01:42 +0900

    JSX内でのJSコードの実行
    JSX内ではJavaScriptの式を{}で囲む
    
    関数を使用して表示する値を指定することもできる
    
    boolean型のデータは表示することが出来ない

--- 04_react_basic/src/075_fragment/start/components/Child.js
+++ 04_react_basic/src/075_fragment/start/components/Child.js
@@ -1,10 +1,15 @@
 import "./Child.css";
+import React from "react";
 
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    <React.Fragment>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id dolorum voluptas odit, quidem ipsam natus, officiis neque unde dolores at qui alias voluptatem in eius, doloremque temporibus earum sed libero.</p>
+    </React.Fragment>
   );
 };
 
---
+++ 04_react_basic/src/080_expr_in_jsx/start/components/Expression.js
@@ -0,0 +1,22 @@
+import "./Expression.css"
+
+const Expression = () => {
+  const title = "Expression";
+  const arry = ['item1', 'item2', 'item3']
+  const hello = (arg) => `${arg} function`;
+  const jsx = <h3>Hello JSX</h3>
+  const bool = true;
+  return (
+    <div className={title.toLowerCase()}>
+      <h3>Hello {title}</h3>
+      <h3>{arry}</h3>
+      <h3>{hello('Hello')}</h3>
+      <h3>{/* 画面上に表示されない */}</h3>
+      {<h3>Hello JSX</h3>}
+      {jsx}
+      {bool}
+    </div>
+  )
+}
+
+export default Expression;
